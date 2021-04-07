12d1dc1b1f8fdb632bee89a3f955c6a5552b949b
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-04-07 03:30:16 -0300

    RP-327 - Pacotes

--- src/Builders/RoleBuilder.php
+++ src/Builders/RoleBuilder.php
@@ -2,35 +2,12 @@
 
 namespace WriteLabel\Builders;
 
-use App\Contants\Tables;
-use WriteLabel\Entities\UserEntity;
-use Illuminate\Database\Eloquent\Builder;
-
 /**
  * Class RoleBuilder.
  *
  * @package WriteLabel\Builders
  */
-class RoleBuilder extends Builder
+class RoleBuilder extends \Population\Manipule\Builders\RoleBuilder
 {
-    /**
-     * @var string
-     */
-    private $rolesTable = Tables::TABLE_ROLES;
-
-    /**
-     * @return $this
-     */
-    public function whereNameCustomer()
-    {
-        return $this->where("{$this->rolesTable}.name", UserEntity::ROLE_CUSTOMER);
-    }
-
-    /**
-     * @return $this
-     */
-    public function whereNameAdministrator()
-    {
-        return $this->where("{$this->rolesTable}.name", UserEntity::ROLE_ADMINISTRATOR);
-    }
+    
 }
