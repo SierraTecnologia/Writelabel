b915bba80405350e7d04d19e756305c59f5f2186
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-04-07 05:59:27 -0300

    RP-327 - Pacotes

--- src/Entities/AbstractEntity.php
+++ src/Entities/AbstractEntity.php
@@ -5,38 +5,12 @@ namespace WriteLabel\Entities;
 use Illuminate\Support\Collection;
 use Illuminate\Contracts\Support\Arrayable;
 use JsonSerializable;
-
 /**
  * Class AbstractEntity.
  *
  * @package Core\Entities
  */
-abstract class AbstractEntity implements Arrayable, JsonSerializable
+abstract class AbstractEntity extends \Muleta\Contracts\Objects\AbstractEntity
 {
-    /**
-     * @inheritdoc
-     */
-    public function toArray(): array
-    {
-        return [];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function jsonSerialize(): array
-    {
-        return $this->toArray();
-    }
-
-    /**
-     * persist
-     *
-     * @return bool
-     */
-    private function persist()
-    {
-        $this->model::create($this->toArray());
-    }
 
 }
